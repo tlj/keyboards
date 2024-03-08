7373f641ef901aed6cf4436d3e673133d1dbfae2
tlj <tlj@tlj.no>
2024-03-08 16:16:12 +0100

    Added combo for arrow (->) and dquo

--- src/qmk/users/tlj/keymaps.c
+++ src/qmk/users/tlj/keymaps.c
@@ -1,5 +1,6 @@
 #include "definitions/keycodes.h"
 #include "definitions/keymap_blocks.h"
+#include "process_combo.h"
 #include QMK_KEYBOARD_H
 #include "tlj.h"
 
@@ -14,41 +15,52 @@ const uint32_t PROGMEM unicode_map[] = {
 
 const uint16_t PROGMEM esc_combo[] = {KC_SHFJ, KC_GUIK, COMBO_END};
 const uint16_t PROGMEM enter_combo[] = {KC_SHFF, KC_GUID, COMBO_END};
-const uint16_t PROGMEM lprn_combo[] = {KC_SHFJ, KC_I, COMBO_END};
-const uint16_t PROGMEM rprn_combo[] = {KC_SHFJ, KC_I, KC_SYMC, COMBO_END};
-const uint16_t PROGMEM lcbr_combo[] = {KC_SHFJ, KC_O, COMBO_END};
-const uint16_t PROGMEM rcbr_combo[] = {KC_SHFJ, KC_O, KC_SYMC, COMBO_END};
-const uint16_t PROGMEM lbrc_combo[] = {KC_SHFJ, KC_COMM, COMBO_END};
-const uint16_t PROGMEM rbrc_combo[] = {KC_SHFJ, KC_COMM, KC_SYMC, COMBO_END};
-const uint16_t PROGMEM mins_combo[] = {KC_SHFJ, KC_GUIK, KC_CTRL, COMBO_END};
-const uint16_t PROGMEM unsc_combo[] = {KC_SHFJ, KC_GUIK, KC_CTRL, KC_SYMC, COMBO_END};
-const uint16_t PROGMEM eql_combo[] = {KC_SHFJ, KC_CTRL, COMBO_END};
-const uint16_t PROGMEM plus_combo[] = {KC_SHFJ, KC_CTRL, KC_SYMC, COMBO_END};
-const uint16_t PROGMEM quot_combo[] = {KC_U, KC_I, COMBO_END};
-const uint16_t PROGMEM dquo_combo[] = {KC_U, KC_I, KC_SYMC, COMBO_END};
+// const uint16_t PROGMEM lprn_combo[] = {KC_SHFJ, KC_I, COMBO_END};
+// const uint16_t PROGMEM rprn_combo[] = {KC_SHFJ, KC_I, KC_SYMC, COMBO_END};
+// const uint16_t PROGMEM lcbr_combo[] = {KC_SHFJ, KC_O, COMBO_END};
+// const uint16_t PROGMEM rcbr_combo[] = {KC_SHFJ, KC_O, KC_SYMC, COMBO_END};
+// const uint16_t PROGMEM lbrc_combo[] = {KC_SHFJ, KC_COMM, COMBO_END};
+// const uint16_t PROGMEM rbrc_combo[] = {KC_SHFJ, KC_COMM, KC_SYMC, COMBO_END};
+// const uint16_t PROGMEM mins_combo[] = {KC_SHFJ, KC_GUIK, KC_CTRL, COMBO_END};
+// const uint16_t PROGMEM unsc_combo[] = {KC_SHFJ, KC_GUIK, KC_CTRL, KC_SYMC, COMBO_END};
+// const uint16_t PROGMEM eql_combo[] = {KC_SHFJ, KC_CTRL, COMBO_END};
+// const uint16_t PROGMEM plus_combo[] = {KC_SHFJ, KC_CTRL, KC_SYMC, COMBO_END};
+// const uint16_t PROGMEM quot_combo[] = {KC_U, KC_I, COMBO_END};
+// const uint16_t PROGMEM dquo_combo[] = {KC_U, KC_I, KC_SYMC, COMBO_END};
 //const uint16_t PROGMEM copy_combo[] = {KC_NORC, KC_V, COMBO_END};
 //const uint16_t PROGMEM past_combo[] = {KC_NORC, KC_V, KC_SYMC, COMBO_END};
 const uint16_t PROGMEM caps_combo[] = {KC_NORC, KC_V, COMBO_END};
 const uint16_t PROGMEM num_word_combo[] = {KC_HNUM, KC_SHFJ, COMBO_END};
+const uint16_t PROGMEM arrow_combo[] = {KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM dquote_combo[] = {KC_M, KC_COMM, COMBO_END};
+
+enum combo_events { 
+    C_ESC,
+    C_ENTER,
+    C_CAPS,
+    C_NUMWORD,
+    C_DQUOTE,
+    C_ARROW,
+};
 
 combo_t key_combos[] = {
-    COMBO(esc_combo, KC_ESC),
-    COMBO(enter_combo, KC_ENT),
-    COMBO(lprn_combo, KC_LPRN),
-    COMBO(rprn_combo, KC_RPRN),
-    COMBO(lcbr_combo, KC_LCBR),
-    COMBO(rcbr_combo, KC_RCBR),
-    COMBO(lbrc_combo, KC_LBRC),
-    COMBO(rbrc_combo, KC_RBRC),
-    COMBO(mins_combo, KC_MINS),
-    COMBO(unsc_combo, KC_UNDS),
-    COMBO(eql_combo, KC_EQL),
-    COMBO(plus_combo, KC_PLUS),
-    COMBO(quot_combo, KC_QUOT),
-    COMBO(dquo_combo, KC_DQUO),
-    COMBO(caps_combo, CW_TOGG),
-    COMBO(num_word_combo, NUMWORD),
+    [C_ESC] = COMBO(esc_combo, KC_ESC),
+    [C_ENTER] = COMBO(enter_combo, KC_ENT),
+    [C_CAPS] = COMBO(caps_combo, CW_TOGG),
+    [C_NUMWORD] = COMBO(num_word_combo, NUMWORD),
+    [C_DQUOTE] = COMBO(dquote_combo, KC_DQUO),
 //    COMBO(copy_combo, LGUI(KC_C)),
 //    COMBO(past_combo, LGUI(KC_V)),
+    [C_ARROW] = COMBO_ACTION(arrow_combo),
 };
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+    switch (combo_index) {
+        case C_ARROW:
+            if (pressed) {
+                SEND_STRING("->");
+            }
+            break;
+    }
+}
 
